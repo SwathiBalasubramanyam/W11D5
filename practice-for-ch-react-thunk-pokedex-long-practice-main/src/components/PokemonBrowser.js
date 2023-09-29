e6ef3c33137402ef098476bd7b7cafbf6750018a
swathi <swathibalasubramanyam@gmail.com>
2023-09-29 14:11:45 -0700

    Completed phase 1

--- practice-for-ch-react-thunk-pokedex-long-practice-main/src/components/PokemonBrowser.js
+++ practice-for-ch-react-thunk-pokedex-long-practice-main/src/components/PokemonBrowser.js
@@ -1,17 +1,23 @@
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Route, useParams } from 'react-router-dom';
 
 import PokemonDetail from './PokemonDetail';
 import CreatePokemonForm from './CreatePokemonForm';
 import Fab from './Fab';
+import { getPokemon } from '../store/pokemon';
 
 const PokemonBrowser = () => {
   const { pokemonId } = useParams();
+  const dispatch = useDispatch();
   const pokemon = useSelector(state => {
     return state.pokemon.list.map(pokemonId => state.pokemon[pokemonId]);
   });
   const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    dispatch(getPokemon())
+  }, [])
 
 
   if (!pokemon) {
